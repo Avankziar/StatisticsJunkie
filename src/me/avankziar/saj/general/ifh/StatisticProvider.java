6780c92903af4d47bbd999f96f4332481ed4f428
Avankziar <avankziar@gmail.com>
2025-03-09 09:07:18 +0100

    1-0-1

--- src/me/avankziar/saj/general/ifh/StatisticProvider.java
+++ src/me/avankziar/saj/general/ifh/StatisticProvider.java
@@ -20,6 +20,10 @@ public class StatisticProvider implements Statistic
 	@Override
 	public Double getStatistic(UUID uuid, StatisticType statisticType)
 	{
+		if(uuid == null || statisticType == null)
+		{
+			return null;
+		}
 		StatisticEntry se = mysql.getData(new StatisticEntry(), 
 				"`player_uuid` = ? AND `statistic_type` = ?",
 				uuid.toString(), statisticType.toString());
@@ -29,6 +33,10 @@ public class StatisticProvider implements Statistic
 	@Override
 	public Double getStatistic(UUID uuid, StatisticType statisticType, String materialOrEntityType)
 	{
+		if(uuid == null || statisticType == null)
+		{
+			return null;
+		}
 		if(materialOrEntityType == null)
 		{
 			return getStatistic(uuid, statisticType);
@@ -42,19 +50,43 @@ public class StatisticProvider implements Statistic
 	@Override
 	public void addStatisticValue(UUID uuid, StatisticType statisticType, String materialOrEntityType, double value)
 	{
-		StatisticEntry se = SAJ.getPlugin().getMysqlHandler().getData(new StatisticEntry(), 
-				"`player_uuid` = ? AND `statistic_type` = ? AND `material_or_entitytype` = ?", 
-				uuid.toString(), statisticType.toString(), materialOrEntityType);
-		if(se != null)
+		if(uuid == null || statisticType == null)
 		{
-			se.setStatisticValue(se.getStatisticValue() + value);
-			SAJ.getPlugin().getMysqlHandler().updateData(se,
-					"`player_uuid` = ? AND `statistic_type` = ? AND `material_or_entitytype` = ?", 
-					uuid.toString(), statisticType.toString(), materialOrEntityType);
+			return;
+		}
+		StatisticEntry se = null;
+		if(materialOrEntityType == null)
+		{
+			se = SAJ.getPlugin().getMysqlHandler().getData(new StatisticEntry(), 
+					"`player_uuid` = ? AND `statistic_type` = ?", 
+					uuid.toString(), statisticType.toString());
+			if(se != null)
+			{
+				se.setStatisticValue(se.getStatisticValue() + value);
+				SAJ.getPlugin().getMysqlHandler().updateData(se,
+						"`player_uuid` = ? AND `statistic_type` = ?", 
+						uuid.toString(), statisticType.toString());
+			} else
+			{
+				se = new StatisticEntry(0, uuid, statisticType, "null", value);
+				SAJ.getPlugin().getMysqlHandler().create(se);
+			}
 		} else
 		{
-			se = new StatisticEntry(0, uuid, statisticType, materialOrEntityType, value);
-			SAJ.getPlugin().getMysqlHandler().create(se);
+			se = SAJ.getPlugin().getMysqlHandler().getData(new StatisticEntry(), 
+					"`player_uuid` = ? AND `statistic_type` = ? AND `material_or_entitytype` = ?", 
+					uuid.toString(), statisticType.toString(), materialOrEntityType);
+			if(se != null)
+			{
+				se.setStatisticValue(se.getStatisticValue() + value);
+				SAJ.getPlugin().getMysqlHandler().updateData(se,
+						"`player_uuid` = ? AND `statistic_type` = ? AND `material_or_entitytype` = ?", 
+						uuid.toString(), statisticType.toString(), materialOrEntityType);
+			} else
+			{
+				se = new StatisticEntry(0, uuid, statisticType, materialOrEntityType, value);
+				SAJ.getPlugin().getMysqlHandler().create(se);
+			}
 		}
 	}
 }
